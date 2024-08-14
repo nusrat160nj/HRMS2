a0bdc03bb3176a569c4e4da065fae27014440548
Soniya <soniya19106208@gmail.com>
2024-08-14 09:31:08 +0530

    pages

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/login.css';
+
+function Login() {
+  return (
+    <div>
+        <div className="box">
+            <div className = "card">
+                <p>Welcome Back!!</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
